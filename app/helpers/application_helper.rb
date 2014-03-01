e0cc3885dbb89e4e58db750aecfe36fd9cc60056
Maksim Yermalovich <yermalovich.m@gmail.com>
2014-03-02 01:45:45 +0300

    add "close" to flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,12 @@ module ApplicationHelper
     flash_html = ''
     flash.each do |key, msg|
       html_class = converter[key] or 'alert-info'
-      flash_html += %Q(<div class="alert #{html_class}">#{msg}</div>)
+      flash_html += %Q(
+        <div class="alert #{html_class}">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          #{msg}
+        </div>
+      )
     end
 
     flash_html.html_safe
